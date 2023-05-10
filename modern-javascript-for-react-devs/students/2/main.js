769ea1f5c982ec84f395c3d4c63892c6188aa15d
CarismaDAqueen <130988079+ccousins694@users.noreply.github.com>
2023-05-10 11:04:05 -0400

    Update ex2 array

--- modern-javascript-for-react-devs/students/2/main.js
+++ modern-javascript-for-react-devs/students/2/main.js
@@ -1,47 +1,47 @@
-/
+
 // File: main.js
 // Date: 5/24/2022
 // this is EX 1 , pushed 5.9.23 // 
 
 
-      // checking to see if ^ is  true statement if (typeof(array[i]== 'number'))
-const ex1 = () => {
-    const array = [1, '2', 3, 'test', 1.2];
-    console.log(countNumbers(array));
+// ex1
+// checking to see if ^ is  true statement if (typeof(array[i]== 'number'))
+// const ex1 = () => {
+//     const array = [1, '2', 3, 'test', 1.2];
+//     console.log(countNumbers(array));
     
  //1st  pass the array, then u use ex 1 to test the array function
+
+
+// ex2
+const minNumber = (array) => {
+    let min = array[0]
+    for (let i = 0; i < array.length; i++){
+        if (array[i] < min) {
+            min = array[i]
+        }
+    }
+    return min;
 }
 
 
-
-// const ex2 = () => {
-//     for (let minNumber = 2; minNumber < 3; minNumber++) {
-//     console.log(minNumber); 
-// }
-// console.log("*".repeat(2));
-// }
-   
-
-// const ex3 = () => {
-    // TODO...
-// }
 
 //
 // Your functions here...
-//
 
-const countNumbers = (array) => {
-    console.log(array)
-    let numberCounter = 0;
-    for (let i=0; i < array.length; i++) {
-        if(typeof(array[i]) == 'number') numberCounter++;
-    } 
-    return numberCounter; 
-}
+// ex1
+// const countNumbers = (array) => {
+//     console.log(array)
+//     let numberCounter = 0;
+//     for (let i=0; i < array.length; i++) {
+//         if(typeof(array[i]) == 'number') numberCounter++;
+//     } 
+//     return numberCounter; 
+// }
 
-const main = async () => {
-    ex1();
-}
+// const main = async () => {
+//     ex1();
+// }
 
 
 // < is an operator
@@ -54,4 +54,19 @@ const main = async () => {
 // iterate means to go thru; repeating a set of instructions over and over; we are looking a the # to determine what it is
 // double equal is 
 
-main();
+//main();
+//
+// ex2
+const ex2 = () => {
+   let array1 = [2, 11, 12, 22, 55, 1];
+   let array2 = [-1, 80, 700];
+        console.log(minNumber(array2));
+}
+ex2();
+
+console.log("*".repeat(2));
+
+
+// const ex3 = () => {
+    // TODO...
+// }
