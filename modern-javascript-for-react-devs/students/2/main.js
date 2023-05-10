910a1faac91c74c9e54676dbc097ca9fa605c091
CarismaDAqueen <130988079+ccousins694@users.noreply.github.com>
2023-05-10 17:43:35 -0400

    Update exercises 2 and 3

--- modern-javascript-for-react-devs/students/2/main.js
+++ modern-javascript-for-react-devs/students/2/main.js
@@ -1,7 +1,7 @@
 
 // File: main.js
-// Date: 5/24/2022
-// this is EX 1 , pushed 5.9.23 // 
+// Created: 5/24/2022
+// this is EX 2 and 3 , pushed 5.10.23 // 
 
 
 // ex1
@@ -9,11 +9,11 @@
 // const ex1 = () => {
 //     const array = [1, '2', 3, 'test', 1.2];
 //     console.log(countNumbers(array));
-    
- //1st  pass the array, then u use ex 1 to test the array function
+
+//1st  pass the array, then u use ex 1 to test the array function
 
 
-// ex2
+ex2
 const minNumber = (array) => {
     let min = array[0]
     for (let i = 0; i < array.length; i++){
@@ -23,11 +23,37 @@ const minNumber = (array) => {
     }
     return min;
 }
+"" = a container
 
+ex3 : usage #1
+const interleave = (ray1, ray2) => { 
+    let result = ""
+    if(ray1.length !== ray2.length){
+    }
+    for (let i=0; i < ray1.length; i++){
+        result += ray1[i]
+        result += ray2 [i]
+    }
+    return result 
+}
+
+// ex3 : usage #2
+const interleave = (ray1, ray2) => {
+    let result = ""
+    if (ray1.length !== ray2.length) {
+        return "Error: Array length mismatch"
+    }
+    for (let i = 0; i < ray1.length; i++) {
+        result += ray1[i]
+        result += ray2[i]
+        //console.log('Error: Array length mismatch') 
+    }
+    return result
+}
 
 
 //
-// Your functions here...
+// Your functions here......................................................................
 
 // ex1
 // const countNumbers = (array) => {
@@ -39,34 +65,49 @@ const minNumber = (array) => {
 //     return numberCounter; 
 // }
 
-// const main = async () => {
-//     ex1();
-// }
 
 
 // < is an operator
- // let a = 1;
-    // let b = 2;
-    // let c = 3; cant do this because array is primitive (cannot be changed)
+// let a = 1;
+// let b = 2;
+// let c = 3; cant do this because array is primitive (cannot be changed)
 
 // ^ this is camelCase (1st word is lowercase and second word is sentence case)
 //retval is a function 
 // iterate means to go thru; repeating a set of instructions over and over; we are looking a the # to determine what it is
 // double equal is 
 
-//main();
-//
-// ex2
+
+ex2
 const ex2 = () => {
    let array1 = [2, 11, 12, 22, 55, 1];
    let array2 = [-1, 80, 700];
         console.log(minNumber(array2));
 }
-ex2();
-
 console.log("*".repeat(2));
 
 
-// const ex3 = () => {
-    // TODO...
-// }
+const ex3 = () => {
+
+    ex3 : usage #1
+
+        let ray1 = [1, 2, 3, 4,5];
+        let ray2 = ['a', 'b', 'c', 'd', 'e'];
+        console.log(interleave(ray1, ray2));
+
+
+    ex3 : usage #2
+
+        let ray1 = [1, 2, 3];
+        let ray2 = ['a', 'b', 'c', 'd', 'e'];
+        console.log(interleave(ray1, ray2));
+
+}
+
+const main = async () => {
+    // ex1();
+    // ex2();
+    ex3();
+}
+
+main();
